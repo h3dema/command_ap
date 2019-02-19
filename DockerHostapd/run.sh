022b5a144120f795c532d6b1a2bcdab0dbf086e3
henriquemoura <henriquemoura@hotmail.com>
2019-02-19 11:31:05 -0300

    compiling

--- DockerHostapd/run.sh
+++ DockerHostapd/run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-if [ ! -d bin ];
+if [ ! -d bin ]; then
     mkdir bin
 fi
 
-docker build -t hostapd .
-docker run -w /WORKDIR -v bin:/WORKDIR/bin -it hostapd
+# identify Ubuntu version
+is_14=`lsb_release -r | grep 14 | wc -l`
+is_16=`lsb_release -r | grep 16 | wc -l`
+if [ "$is_14" -eq 1 ]; then
+    docker build -t hostapd ./14
+elif [ "$is_16" -eq 1 ]; then
+    docker build -t hostapd ./16
+fi
+
+docker run -w /WORKDIR -v "`pwd`/bin":/vol -it hostapd
+#"cp /WORKDIR/bin/* /tmp"
+
