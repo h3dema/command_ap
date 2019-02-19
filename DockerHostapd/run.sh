d4f923b12c0bb444eeb7cfb88e41bae663b81c57
henriquemoura <henriquemoura@hotmail.com>
2019-02-19 12:13:10 -0300

    ubuntu 18 dockerfile

--- DockerHostapd/run.sh
+++ DockerHostapd/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+IDS=`docker ps | grep hostapd | awk '{print $1}'`
+for id in $IDS; do
+    docker stop $id
+done
+
 if [ ! -d bin ]; then
     mkdir bin
 fi
@@ -13,6 +19,20 @@ elif [ "$is_16" -eq 1 ]; then
     docker build -t hostapd ./16
 fi
 
-docker run -w /WORKDIR -v "`pwd`/bin":/vol -it hostapd
-#"cp /WORKDIR/bin/* /tmp"
 
+
+docker_hostapd_running=`docker ps | grep hostapd | wc -l`
+if [ "$docker_hostapd_running" -eq "0" ]; then
+    docker run -w /WORKDIR -t -d hostapd
+fi
+
+docker ps
+
+ID=`docker ps | grep hostapd | awk '{print $1}'`
+docker cp $ID:/WORKDIR/hostap/hostapd/hostapd bin
+docker cp $ID:/WORKDIR/hostap/hostapd/hostapd_cli bin
+docker cp $ID:/WORKDIR/iw/iw bin
+
+docker stop $ID
+
+ls bin
