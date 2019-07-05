735e53bd29f6bc3c84b2077659024f10c33a1db4
Marcos Carvalho <marcosf40@hotmail.com>
2019-07-05 14:57:18 -0300

    create_window

--- rl/model.py
+++ rl/model.py
@@ -6,8 +6,21 @@
 """
 
 import keras
-
+import numpy as np
 
 def get_QoS(features):
     raise Exception("Implement get_QoS")
     pass
+
+def create_window(data_values, __timesteps):
+    data_values = data_values.reshape(data_values.shape[0], 1, data_values.shape[1])
+    ds = np.zeros((1, __timesteps, data_values.shape[2]))
+    for j in range(0, data_values.shape[0] - __timesteps + 1):
+
+        window = data_values[j:j + 1, :, :]
+
+        for k in range(1, __timesteps):
+            window = np.append(window, data_values[j + k:j + k + 1, :, :], axis=1)
+
+        ds = np.append(ds, window, axis=0)
+    return ds[1:, :, :]
