162fc7fba013303c96c29f98fa95a6412daef910
henriquemoura <henriquemoura@hotmail.com>
2019-11-13 18:15:48 -0300

    clean up

--- rl/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-curr = os.getcwd()
-
-if curr not in sys.path():
-    sys.path.append(curr)
--- rl/app1.py
+++
@@ -1,9 +0,0 @@
-import logging as log
-from basic.environment import environment
-
-log.basicConfig(level=log.INFO)
-
-if __name__ == "__main__":
-    e = environment()
-    e.get_reward()
-    e.take_action()
